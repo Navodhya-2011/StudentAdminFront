e6c7863e3f3734dcda57ff1b2fd71b013123170a
“[Navodhya <navodhya2011@gmail.com>
2022-07-07 16:07:20 +0530

    Insert index.jsx changes

--- src/Pages/Insert/index.jsx
+++ src/Pages/Insert/index.jsx
@@ -4,10 +4,6 @@ import axios from "axios";
 function InsertNote(){
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => {
-        setIsOpen(!isOpen);
-    };
-
     const [authId, setAuthId] = useState("");
     const [title, setTitle] = useState("");
     const [text, setText] = useState("");
@@ -70,6 +66,57 @@ function InsertNote(){
                                 }
                             />
                         </div>
+                        <div class="mb-6">
+                            <label
+                                class="block text-gray-700 text-sm font-bold mb-2"
+                                for="username">
+                                Title
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-1 focus:outline-green-300 focus:shadow-outline"
+                                id="username"
+                                type="text"
+                                placeholder="Author ID"
+                                onChange={(e) =>
+                                    setTitle(e.target.value)
+                                }
+                            />
+                        </div>
+                        <div class="mb-6">
+                            <label
+                                class="block text-gray-700 text-sm font-bold mb-2"
+                                for="username">
+                                Text
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-1 focus:outline-green-300 focus:shadow-outline"
+                                id="username"
+                                type="text"
+                                placeholder="Text"
+                                onChange={(e) =>
+                                    setText(e.target.value)
+                                }
+                            />
+                        </div>
+                        <div class="mb-6">
+                            <label
+                                class="block text-gray-700 text-sm font-bold mb-2"
+                                for="username">
+                                Sender
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-1 focus:outline-green-300 focus:shadow-outline"
+                                id="username"
+                                type="text"
+                                placeholder="Sender"
+                                onChange={(e) =>
+                                    setSender(e.target.value)
+                                }
+                            />
+                        </div>
+                        <button class="bg-green-600 mx-48 mt-4 hover:bg-green-700 text-white font-bold py-2 px-24 rounded">
+                            Submit
+                        </button>
                     </form>
                 </div>    
             </div>       
