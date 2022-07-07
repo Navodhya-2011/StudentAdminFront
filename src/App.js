9ebea78261d78814f9e33688fca1a5bdd2346344
“[Navodhya <navodhya2011@gmail.com>
2022-07-07 23:50:50 +0530

    App.js Implementation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 import AdminSignup from "./components/AdminSignUp";
 import InsertNote from "./Pages/Insert";
 import UpdateStudent from "./Pages/Update";
+import StudentNote from "./Pages/StudentNote";
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
       <Route path="/adminsignup" element={<AdminSignup />} />
       <Route path="/insert" element={<InsertNote />} />
       <Route path="/noteUpdate" element={<UpdateStudent />} />
+      <Route path="/studentnote" element={<StudentNote />} />
     </Routes>
   );
 }
